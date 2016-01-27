7a931f4cb345ab22959f8f79d9595a7b4cfe31b5
Arfon Smith <arfon@github.com>
2016-01-27 14:30:59 -0500

    Removing unused Sessions views

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :papers
 
   get '/about', :to => 'home#about', :as => 'about'
-  get '/sessions/new', :to => 'sessions#new', :as => 'new_session'
   get '/auth/:provider/callback', :to => 'sessions#create'
   get "/signout" => "sessions#destroy", :as => :signout
 

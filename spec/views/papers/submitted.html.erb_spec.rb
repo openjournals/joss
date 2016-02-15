3591d1d9e739ba434d51d73d94e2af6e58a95f18
Arfon Smith <arfon@github.com>
2016-02-14 21:42:12 -0700

    Moar view tests

---
+++ spec/views/papers/submitted.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'papers/submitted.html.erb' do
+  context 'for submitted papers' do
+    it "should show the correct number of papers" do
+      3.times do
+        create(:paper, :state => "accepted")
+      end
+
+      create(:paper, :state => "submitted")
+
+      assign(:papers, Paper.submitted.paginate(:page => 1, :per_page => 10))
+
+      render :template => "papers/index.html.erb"
+
+      expect(rendered).to have_selector('.paper-title', :count => 1)
+      expect(rendered).to have_content "Submitted papers (1)"
+    end
+  end
+end

77dde95fd3d83bc1eec34a7fb6286b50f426617c
Arfon Smith <arfon.smith@gmail.com>
2016-05-06 22:29:08 -0600

    Fixes #80

--- app/helpers/papers_helper.rb
+++ app/helpers/papers_helper.rb
@@ -4,4 +4,20 @@ module PapersHelper
       "selected"
     end
   end
+
+    def formatted_body(paper, length=nil)
+    pipeline = HTML::Pipeline.new [
+      HTML::Pipeline::MarkdownFilter,
+      HTML::Pipeline::SanitizationFilter
+    ]
+
+    if length
+      body = paper.body.truncate(length, separator: /\s/)
+    else
+      body = paper.body
+    end
+    
+    result = pipeline.call(body)
+    result[:output].to_s.html_safe
+  end
 end

2398b899b750c2993672d225a9b6666d6eea4e3a
Arfon Smith <arfon.smith@gmail.com>
2016-09-16 12:42:49 -0700

    Fixing submission URLs

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -2,7 +2,7 @@ class Notifications < ApplicationMailer
   EDITOR_EMAILS = ["joss.theoj@gmail.com "]
 
   def submission_email(paper)
-    @url  = "http://joss.theoj.org/#{paper.sha}"
+    @url  = "http://joss.theoj.org/papers/#{paper.sha}"
     @paper = paper
     mail(:to => EDITOR_EMAILS, :subject => "New submission: #{paper.title}")
   end

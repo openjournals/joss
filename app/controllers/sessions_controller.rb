08ac991a6e15770c5de46545f26db97c1c00dc9e
Arfon Smith <arfon.smith@gmail.com>
2017-12-06 11:12:46 -0500

    env -> ENV

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
+    user = User.from_omniauth(ENV["omniauth.auth"])
 
     # Get the user's name from ORCID API
     # OrcidWorker.perform_async(user.uid)

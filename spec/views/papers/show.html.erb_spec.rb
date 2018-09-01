69eda9f20e2424fc58fada714d5270d98d042449
Arfon Smith <arfon.smith@gmail.com>
2018-09-01 13:53:25 -0400

    Adding author email to admin view

--- spec/views/papers/show.html.erb_spec.rb
+++ spec/views/papers/show.html.erb_spec.rb
@@ -10,7 +10,7 @@ describe 'papers/show.html.erb' do
       user = create(:user)
       allow(view).to receive_message_chain(:current_user).and_return(user)
 
-      paper = create(:paper, :state => "submitted")
+      paper = create(:paper, :state => "submitted", :submitting_author => user)
       assign(:paper, paper)
 
       render :template => "papers/show.html.erb"
@@ -34,14 +34,14 @@ describe 'papers/show.html.erb' do
   context 'rendering admin partial' do
     it "displays buttons when there's no GitHub issue" do
       user = create(:user, :admin => true)
+      author = create(:user)
       allow(view).to receive_message_chain(:current_user).and_return(user)
 
-      paper = create(:paper, :state => "submitted", :review_issue_id => nil)
+      paper = create(:paper, :state => "submitted", :review_issue_id => nil, :submitting_author => author)
       assign(:paper, paper)
 
       render :template => "papers/show.html.erb"
 
-      expect(rendered).to have_selector("input[type=submit][value='Start review']")
       expect(rendered).to have_selector("input[type=submit][value='Reject paper']")
     end
 
@@ -58,20 +58,24 @@ describe 'papers/show.html.erb' do
 
     it "shows the withdraw button to admins" do
       user = create(:user, :admin => true)
+      author = create(:user)
       allow(view).to receive_message_chain(:current_user).and_return(user)
 
-      paper = create(:paper, :state => "submitted")
+      paper = create(:paper, :state => "submitted", :submitting_author => author)
       assign(:paper, paper)
 
       render :template => "papers/show.html.erb"
       expect(rendered).to have_selector("input[type=submit][value='Withdraw paper']")
+      expect(rendered).to have_content(author.email)
     end
 
     it "doesn't displays buttons when there's a GitHub issue" do
       user = create(:user, :admin => true)
+      author = create(:user)
+
       allow(view).to receive_message_chain(:current_user).and_return(user)
 
-      paper = create(:paper, :state => "submitted", :review_issue_id => 123)
+      paper = create(:paper, :state => "submitted", :review_issue_id => 123, :submitting_author => author)
       assign(:paper, paper)
 
       render :template => "papers/show.html.erb"

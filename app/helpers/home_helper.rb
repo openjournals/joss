ed430e140f68d998d2c38518cabbf18ad323dfeb
Arfon Smith <arfon.smith@gmail.com>
2018-10-03 08:25:22 -0400

    Rescue helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -5,7 +5,7 @@ module HomeHelper
   end
 
   def card_activity(paper)
-    return "No activity" if paper.activities.nil?
+    return "No activity" if paper.activities.empty?
     return "No activity" if paper.activities['issues']['commenters'].empty?
     capture do
       if !paper.activities['issues']['comments'].empty?

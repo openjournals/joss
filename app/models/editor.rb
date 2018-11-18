f4a31e1518050acf760acfeceacc4b941b623386
Arfon Smith <arfon.smith@gmail.com>
2018-11-17 20:45:45 -0500

    Adding emeritus role for editors

--- app/models/editor.rb
+++ app/models/editor.rb
@@ -1,8 +1,8 @@
 class Editor < ActiveRecord::Base
-  validates :kind, presence: true, inclusion: { in: ["board", "topic"] }
+  validates :kind, presence: true, inclusion: { in: ["board", "topic", "emeritus"] }
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :login, presence: true
+  validates :login, presence: true, unless: Proc.new { |editor| editor.kind == "emeritus"  }
 
   belongs_to :user
   has_many :papers
@@ -10,8 +10,15 @@ class Editor < ActiveRecord::Base
   before_save :clear_title, if: :board_removed?
   before_save :format_login, if: :login_changed?
 
+  ACTIVE_EDITOR_STATES = [
+    "board",
+    "topic"
+  ].freeze
+
   scope :board, -> { where(kind: "board") }
   scope :topic, -> { where(kind: "topic") }
+  scope :emeritus, -> { where(kind: "emeritus") }
+  scope :active, -> { where(:kind => ACTIVE_EDITOR_STATES) }
 
   def category_list
     categories.join(", ")

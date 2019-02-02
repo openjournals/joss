1e7d60e204638fa88a8a98235efaf9047bbb1c23
Arfon Smith <arfon.smith@gmail.com>
2019-02-01 22:03:53 -0500

    Paper ordering

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -48,7 +48,7 @@ class PapersController < ApplicationController
 
   def popular
     # TODO: Need to order by popularity here
-    @papers = Paper.visible.paginate(
+    @papers = Paper.visible.order(:accepted_at => :desc).paginate(
                 :page => params[:page],
                 :per_page => 10
               )

0b0949040aa0f4a12a306da31e9589fb574de673
Arfon Smith <arfon@users.noreply.github.com>
2019-08-12 14:23:26 -0400

    Update paper_spec.rb

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -164,7 +164,6 @@ describe Paper do
       it { is_expected.to match /Does the repository contain a plain-text LICENSE file/ }
       it { is_expected.to match /Does installation proceed as outlined/ }
       it { is_expected.to match /Are there automated tests/ }
-      it { is_expected.to match /Does the `paper.md` file include a list of authors/ }
     end
   end
 

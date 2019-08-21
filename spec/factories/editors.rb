cbccaa1dc7c52791c5658cbce8595acfc8d73452
Juanjo Bazán <jjbazan@gmail.com>
2019-08-21 11:53:50 +0200

    update factories

--- spec/factories/editors.rb
+++ spec/factories/editors.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :editor do
     user  { create(:user) }
-    kind "topic"
-    first_name "Person"
-    last_name "McEditor"
-    login "mceditor"
-    email "mceditor@example.com"
-    avatar_url "http://placekitten.com/g/200"
-    categories %w(topic1 topic2 topic3)
-    url "http://placekitten.com"
-    description "Person McEditor is an editor"
+    kind { "topic" }
+    first_name { "Person" }
+    last_name { "McEditor" }
+    login { "mceditor" }
+    email { "mceditor@example.com" }
+    avatar_url { "http://placekitten.com/g/200" }
+    categories { %w(topic1 topic2 topic3) }
+    url { "http://placekitten.com" }
+    description { "Person McEditor is an editor" }
   end
 end

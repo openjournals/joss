61af62df2c593902dbd62d94243afbf0496c6c34
Arfon Smith <arfon.smith@gmail.com>
2020-01-30 15:28:47 -0500

    Published not accepted

--- spec/views/papers/show.html.erb_spec.rb
+++ spec/views/papers/show.html.erb_spec.rb
@@ -31,7 +31,7 @@ describe 'papers/show.html.erb' do
       # FIXME - these tests seem to be timezone sensitive??? i.e. can fail depending
       # upon the time of day when running the tests...
       expect(rendered).to have_content "Submitted #{Time.now.strftime('%d %B %Y')}"
-      expect(rendered).to have_content "Accepted #{Time.now.strftime('%d %B %Y')}"
+      expect(rendered).to have_content "Published #{Time.now.strftime('%d %B %Y')}"
       expect(rendered).to have_content "Ruby"
       expect(rendered).to have_content "Editor: @arfon (all papers)"
       expect(rendered).to have_content "Reviewers: @jim (all reviews), @jane (all reviews)"

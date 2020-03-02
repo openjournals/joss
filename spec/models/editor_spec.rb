abd4586e6373d354f3c654d300de4ddc66db9d6b
Arfon Smith <arfon@users.noreply.github.com>
2020-03-02 18:42:02 -0500

    Dashboard refresh (#695)
    * Freshening up editor dashboard styles
    
    * CSS ellipsis
    
    * Chevron order
    
    * Ignore Whedon activities
    
    * More mobile friendly
    
    * UI tweaks
    
    * Heading titles
    
    * Formatting dashboard
    
    * Layout
    
    * Clean up UI
    
    * Finishing up dashboard improvements
    
    * Assign the editor
    
    * Assign the editor
    
    * Assign the editor

--- spec/models/editor_spec.rb
+++ spec/models/editor_spec.rb
@@ -45,6 +45,48 @@ RSpec.describe Editor, type: :model do
     end
   end
 
+  describe "#three_month_average" do
+    before(:each) {
+      create(:accepted_paper, :editor => editor, :accepted_at => 1.week.ago);
+      create(:accepted_paper, :editor => editor, :accepted_at => 3.weeks.ago);
+      create(:accepted_paper, :editor => editor, :accepted_at => 4.months.ago)
+    }
+
+    it "should know how many papers the editor has published" do
+      expect(editor.papers.count).to eq(3)
+    end
+
+    it "should know how to calculate the monntly average papers" do
+      expect(editor.three_month_average).to eq("0.7")
+    end
+  end
+
+  describe "#global editor stats" do
+    before do
+      # This editor should be ignored as they're retired
+      retired_editor = create(:editor, login: "@retired1", kind: "emeritus")
+      create(:accepted_paper, :editor => retired_editor, :accepted_at => 1.week.ago)
+      create(:accepted_paper, :editor => retired_editor, :accepted_at => 1.year.ago)
+
+      # This editor should be ignored as they're brand new
+      new_editor = create(:editor, login: "@topic1", kind: "topic")
+      create(:accepted_paper, :editor => new_editor, :accepted_at => 1.week.ago)
+
+      # These editors should be the ones that count
+      active_editor_1 = create(:editor, login: "@topic1", kind: "topic", :created_at => 4.months.ago)
+      create(:accepted_paper, :editor => active_editor_1, :accepted_at => 1.week.ago) #counts
+      create(:accepted_paper, :editor => active_editor_1, :accepted_at => 1.year.ago) #doesn't count
+
+      active_editor_2 = create(:editor, login: "@topic1", kind: "topic", :created_at => 4.years.ago)
+      create(:accepted_paper, :editor => active_editor_2, :accepted_at => 1.week.ago) #counts
+      create(:accepted_paper, :editor => active_editor_2, :accepted_at => 1.month.ago) #counts
+    end
+
+    it "should know how to calculate the overall #global_three_month_average" do
+      expect(Editor.global_three_month_average).to eq("0.5")
+    end
+  end
+
   describe "#active editors" do
     it "should exclude emeritus" do
       editor_1 = create(:editor, login: "@board1", kind: "board")

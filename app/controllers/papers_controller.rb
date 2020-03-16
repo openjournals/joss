d05be01572aaadc6bc1c3e4aae4e62e0d671fff3
Juanjo Bazán <jjbazan@gmail.com>
2020-03-16 18:47:02 +0100

    disable misspellings in people filters

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -107,21 +107,21 @@ class PapersController < ApplicationController
       @term = "in #{params['language']}"
 
     elsif params['author']
-      @papers = Paper.search(params['author'], fields: [:authors], order: { accepted_at: :desc },
+      @papers = Paper.search(params['author'], fields: [:authors], misspellings: false, order: { accepted_at: :desc },
                   page: params[:page],
                   per_page: 10
                 )
       @term = "by #{params['author']}"
 
     elsif params['editor']
-      @papers = Paper.search(params['editor'], fields: [:editor], order: { accepted_at: :desc },
+      @papers = Paper.search(params['editor'], fields: [:editor], misspellings: false, order: { accepted_at: :desc },
                   page: params[:page],
                   per_page: 10
                 )
       @term = "edited by #{params['editor']}"
 
     elsif params['reviewer']
-      @papers = Paper.search(params['reviewer'], fields: [:reviewers], order: { accepted_at: :desc },
+      @papers = Paper.search(params['reviewer'], fields: [:reviewers], misspellings: false, order: { accepted_at: :desc },
                   page: params[:page],
                   per_page: 10
                 )

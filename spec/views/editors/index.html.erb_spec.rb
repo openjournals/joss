68c021ec4d2d7ed2d09c4eb8e2f5749b3ad2ddaf
Arfon Smith <arfon.smith@gmail.com>
2020-03-24 16:26:17 -0400

    Fixing broken spec

--- spec/views/editors/index.html.erb_spec.rb
+++ spec/views/editors/index.html.erb_spec.rb
@@ -7,10 +7,8 @@ RSpec.describe "editors/index", type: :view do
 
   it "renders a list of editors" do
     render
-    assert_select "tr>td:nth-of-type(1)", text: "topic", count: 2
-    assert_select "tr>td:nth-of-type(2)", text: "Person", count: 2
-    assert_select "tr>td:nth-of-type(3)", text: "McEditor", count: 2
-    assert_select "tr>td:nth-of-type(4)", text: "mceditor", count: 2
-    assert_select "tr>td:nth-of-type(5)", text: "topic1, topic2, topic3", count: 2
+    assert_select "tr>td:nth-of-type(1)", text: "Person McEditor", count: 2
+    assert_select "tr>td:nth-of-type(2)", text: "mceditor", count: 2
+    assert_select "tr>td:nth-of-type(3)", text: "topic1, topic2, topic3", count: 2
   end
 end

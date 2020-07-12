c0ae633f7d8a47723fe7405a702d4ce7cd58b6cd
Arfon Smith <arfon.smith@gmail.com>
2020-07-12 19:46:23 -0400

    Allow comments without votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,12 +4,14 @@ class Vote < ActiveRecord::Base
 
   VOTE_KINDS = [
     "in-scope",
-    "out-of-scope"
+    "out-of-scope",
+    "comment"
   ].freeze
 
   default_scope  { order(created_at: :desc) }
   scope :in_scope, lambda { where('kind = ?', 'in-scope') }
   scope :out_of_scope, lambda { where('kind = ?', 'out-of-scope') }
+  scope :comment_only, lambda { where('kind = ?', 'comment') }
 
   def in_scope?
     kind == "in-scope"
@@ -17,6 +19,10 @@ class Vote < ActiveRecord::Base
 
   def out_of_scope?
     kind == "out-of-scope"
+  end
+
+  def comment?
+    kind == "comment"
   end
 
   validates :kind, inclusion: { in: VOTE_KINDS }, allow_nil: false

a7404ddd46db2744103d99e7a702d74a5c51fdd1
Elizabeth DuPre <emd222@cornell.edu>
2020-09-21 17:19:25 -0400

    Update seeded editors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,28 @@
 Editor.create(
   kind: "board",
   title: "Editor-in-Chief",
-  first_name: "Pierre",
-  last_name: "Bellec",
-  login: "pbellec",
+  first_name: "Elizabeth",
+  last_name: "DuPre",
+  login: "emdupre",
   email: "",
-  avatar_url: "https://avatars3.githubusercontent.com/u/1670887?s=460&u=a1e38ebcf301f9639ed1a2bf8d6ff081bbbda489&v=4",
-  categories: [],
-  url: "https://simexp.github.io/lab-website/",
+  avatar_url: "https://elizabeth-dupre.com/static/img/me.3042ad9.jpg",
+  categories: ["Who even knows."],
+  url: "https://elizabeth-dupre.com/",
   description: <<-STR.strip_heredoc()
+    The raw energy did in fact travel through the phone speaker and directly into my brain,
+    killing me instantly.
   STR
 )
 
 Editor.create(
   kind: "topic",
-  first_name: "Agah",
-  last_name: "Karakuzu",
-  login: "agahkarakuzu",
+  first_name: "Pierre",
+  last_name: "Bellec",
+  login: "pbellec",
   email: "",
-  avatar_url: "https://avatars0.githubusercontent.com/u/9632322?s=460&u=6cce16037ef945d2061ed4d2888c402de3f4b82f&v=4",
-  categories: ["Computational Science and Engineering", "High-performance Computing"],
-  url: "http://lorenabarba.com/",
+  avatar_url: "https://avatars3.githubusercontent.com/u/1670887?s=460&u=a1e38ebcf301f9639ed1a2bf8d6ff081bbbda489&v=4",
+  categories: ["Machine learning", "Shinobi III"],
+  url: "https://simexp.github.io/lab-website/",
   description: <<-STR.strip_heredoc()
   STR
 )

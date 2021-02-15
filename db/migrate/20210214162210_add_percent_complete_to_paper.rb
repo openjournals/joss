041902eddc4e58ef5d99a0df9a967d07bad88b2b
Arfon Smith <arfon@github.com>
2021-02-15 11:22:39 +0000

    Remove migration step

--- db/migrate/20210214162210_add_percent_complete_to_paper.rb
+++ db/migrate/20210214162210_add_percent_complete_to_paper.rb
@@ -2,10 +2,5 @@ class AddPercentCompleteToPaper < ActiveRecord::Migration[6.1]
   def change
     add_column :papers, :percent_complete, :float, default: 0.0
     add_index :papers, :percent_complete
-
-    # Migrate past papers
-    Paper.all.each do |paper|
-      paper.submission_kind = 'new'; paper.save
-    end
   end
 end

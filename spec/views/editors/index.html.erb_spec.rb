c07ab94f6632851ece051c90ef01e2110285969a
Juanjo Bazán <jjbazan@gmail.com>
2021-03-09 11:36:33 +0100

    add description column to editors' table

--- spec/views/editors/index.html.erb_spec.rb
+++ spec/views/editors/index.html.erb_spec.rb
@@ -9,8 +9,9 @@ RSpec.describe "editors/index", type: :view do
     render
     assert_select "tr>td:nth-of-type(1)", text: /Person McEditor/, count: 2
     assert_select "tr>td:nth-of-type(2)", text: "mceditor", count: 2
-    assert_select "tr>td:nth-of-type(3)", text: "topic1, topic2, topic3", count: 2
-    assert_select "tr>td:nth-of-type(5)", text: "🟢*", count: 2
-    assert_select "tr>td:nth-of-type(5)>span[title=?]", "Available: OOO until March 1", count: 2
+    assert_select "tr>td:nth-of-type(3)", text: "Person McEditor is an editor", count: 2
+    assert_select "tr>td:nth-of-type(4)", text: "topic1, topic2, topic3", count: 2
+    assert_select "tr>td:nth-of-type(6)", text: "🟢*", count: 2
+    assert_select "tr>td:nth-of-type(6)>span[title=?]", "Available: OOO until March 1", count: 2
   end
 end

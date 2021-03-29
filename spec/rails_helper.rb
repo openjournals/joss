00997e08145534f4511bfce46b2460004c48b9b0
Juanjo Bazán <jjbazan@gmail.com>
2021-03-29 19:25:16 +0200

    use system specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'ostruct'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in

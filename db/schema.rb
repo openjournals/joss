d9ec6e194a45a242ec334aa1a80441de536ba04d
Arfon Smith <arfon.smith@gmail.com>
2021-07-06 11:54:20 +0100

    Adding version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_101009) do
+ActiveRecord::Schema.define(version: 2021_07_06_101511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"

9c10061aace8a35a9e8f12f880a77d0073bafc15
Juanjo Bazán <jjbazan@gmail.com>
2022-02-21 13:40:44 +0100

    set test token

--- spec/lib/repository_spec.rb
+++ spec/lib/repository_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe Repository do
       expect(Octokit).to receive(:user).and_return(OpenStruct.new(id: 33))
       expected_url = "https://api.github.com/orgs/openjournals/invitations"
       expected_params = {invitee_id: 33, team_ids: [1234]}
-      exheaders = {"Authorization" => "token ", "Content-Type" => "application/json", "Accept" => "application/vnd.github.v3+json"}
+      exheaders = {"Authorization" => "token testGHtoken", "Content-Type" => "application/json", "Accept" => "application/vnd.github.v3+json"}
       expect(Faraday).to receive(:post).with(expected_url, expected_params.to_json, exheaders).and_return(OpenStruct.new(status: 404))
 
       expect(Repository.invite_to_editors_team("unexistent_user")).to be false
@@ -66,7 +66,7 @@ RSpec.describe Repository do
       expect(Octokit).to receive(:user).and_return(OpenStruct.new(id: 42))
       expected_url = "https://api.github.com/orgs/openjournals/invitations"
       expected_params = {invitee_id: 42, team_ids: [1234]}
-      exheaders = {"Authorization" => "token ", "Content-Type" => "application/json", "Accept" => "application/vnd.github.v3+json"}
+      exheaders = {"Authorization" => "token testGHtoken", "Content-Type" => "application/json", "Accept" => "application/vnd.github.v3+json"}
       expect(Faraday).to receive(:post).with(expected_url, expected_params.to_json, exheaders).and_return(OpenStruct.new(status: 200))
 
       expect(Repository.invite_to_editors_team("unexistent_user")).to be true

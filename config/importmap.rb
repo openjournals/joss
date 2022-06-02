1ed65511828c3b4d17fed6eb6aab9e52f2691b5c
Juanjo Bazán <jjbazan@gmail.com>
2022-06-02 14:43:39 +0200

    Use ClipboardJS directly (pin fails)

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,9 +8,6 @@ pin_all_from "app/javascript/custom", under: "custom"
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
 
-# ClipboardJS
-pin "clipboard.js", to: "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.10/clipboard.min.js", preload: true
-
 # JQuery
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js", preload: true
 pin "jquery-ujs", to: "https://ga.jspm.io/npm:jquery-ujs@1.2.3/src/rails.js", preload: true

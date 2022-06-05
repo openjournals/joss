c14886f5801ed54dd74632364e71042676921e03
Arfon Smith <arfon@github.com>
2022-06-05 14:09:13 +0100

    Adding initial tracks

---
+++ spec/lib/tracks_spec.rb
@@ -0,0 +1,50 @@
+RSpec.describe do
+  let(:reference_tracks) do
+    YAML.load_file('spec/fixtures/reference-tracks.yml')
+  end
+
+  let(:joss_tracks) do 
+    YAML.load_file('lib/tracks.yml')
+  end
+
+  describe "Reference tracks" do
+    it "should be 333 in total" do
+      expect(reference_tracks.size).to eq(332)
+    end
+
+    it "should be not have any dupes" do
+      expect(reference_tracks.uniq.size).to eq(332)
+    end
+  end
+
+  describe "JOSS tracks" do
+    it "should have 7 top level tracks" do
+      expect(joss_tracks['tracks'].size).to eq(7)
+    end
+
+    it "should have the right structure" do
+      expected_values = ['name', 'short_name', 'eics', 'code', 'fields']
+      joss_tracks['tracks'].each_pair do |track_slug, track_values|
+        expect(track_values.keys).to eq(expected_values)
+        expect(track_values['eics']).to be_a(Array)
+        expect(track_values['fields']).to be_a(Array)
+        expect(track_values['eics']).not_to be_empty
+        expect(track_values['fields']).not_to be_empty
+      end
+    end
+
+    it "should have no dupes" do
+      all_tracks = joss_tracks['tracks'].collect {|k,v| v['fields']}.flatten.uniq
+
+      expect(all_tracks.size).to eq(332)
+    end
+
+    it "should include all of the reference tracks" do
+      all_tracks = joss_tracks['tracks'].collect {|k,v| v['fields']}.flatten.uniq
+
+      reference_tracks.each do |track|
+        expect(all_tracks.include?(track)).to be(true), "Track #{track} is missing"
+      end
+    end
+  end
+end

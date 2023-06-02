5fb12ca69cf2e6d3c9ef1cfd88ff3791ae35e689
Juanjo Bazán <jjbazan@gmail.com>
2023-06-02 11:29:33 +0200

    Fix active papers counter

--- spec/views/papers/submitted.html.erb_spec.rb
+++ spec/views/papers/submitted.html.erb_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'papers/submitted.html.erb' do
   before { skip_paper_repo_url_check }
 
-  context 'for submitted papers' do
-    it "should show the correct number of papers" do
+  context 'submitted papers' do
+    it "should not show" do
       user = create(:user)
 
       3.times do
@@ -18,7 +18,8 @@ describe 'papers/submitted.html.erb' do
       render template: "papers/index", formats: :html
 
       expect(rendered).to have_selector('.paper-title', count: 0)
-      expect(rendered).to have_content(:visible, "Active Papers 1", normalize_ws: true)
+      expect(rendered).to have_content(:visible, "All Papers 3", normalize_ws: true)
+      expect(rendered).to have_content(:visible, "Active Papers 0", normalize_ws: true)
     end
   end
 end

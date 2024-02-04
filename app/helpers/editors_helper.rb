d9629f3c6cf8447e0e34fcb35fec0f6d5ac7ea00
Arfon Smith <arfon.smith@gmail.com>
2024-02-04 21:48:37 +0000

    Editor dashboard tweaks

--- app/helpers/editors_helper.rb
+++ app/helpers/editors_helper.rb
@@ -16,12 +16,13 @@ module EditorsHelper
 
     comment = "#{editor.max_assignments} max."
 
-    display_count = editor.max_assignments
+    display_count = "#{active_assignments} / #{editor.max_assignments}"
 
     if editor.availability_comment.present?
-      display_count = "#{display_count}*"
+      display_count = "#{active_assignments} / #{editor.max_assignments}*"
       comment += " : #{editor.availability_comment}"
     end
+    
 
     "<span class='#{availability_class}' title='#{comment}' %>#{display_count}</span>".html_safe
   end
@@ -31,6 +32,11 @@ module EditorsHelper
     availability = editor.max_assignments - active_assignments
 
     return "#{availability}"
+  end
+
+  def active_assignments_for_editor(editor)
+    active_assignments = @assignment_by_editor[editor.id].to_i - @paused_by_editor[editor.id].to_i
+    return "#{active_assignments}"
   end
 
   def in_progress_for_editor(editor)

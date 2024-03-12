89efe37271e168ed09dcf907d1a5100bf7a11f49
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-03-12 16:08:28 -0700

    remove recommonmark import

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import recommonmark
-from recommonmark.transform import AutoStructify
 
 # -- Project information -----------------------------------------------------
 
